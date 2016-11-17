fd2be52ef5b2b335411c45766a8dd5902d2f88bc
eugen <eugen922@yahoo.de>
2016-11-17 15:58:53 +0100

    User models but maybe need imports to it

--- polls/Models/user.py
+++ polls/Models/user.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    username = models.CharField(max_length= 100)
-    password = models.CharField(max_length= 100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
 
     def __str__(self):
         return self.username
