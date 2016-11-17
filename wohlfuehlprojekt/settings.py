65f63b34e1707eeaa1e2236d8ccdfbb688ba2822
srose1 <srose@fh-bielefeld.de>
2016-11-17 14:27:04 +0100

    update gitignore add some files

--- wohlfuehlprojekt/settings.py
+++ wohlfuehlprojekt/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'wohlfühlprojekt.urls'
+ROOT_URLCONF = 'wohlfuehlprojekt.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wohlfühlprojekt.wsgi.application'
+WSGI_APPLICATION = 'wohlfuehlprojekt.wsgi.application'
 
 
 # Database
--- wohlfuehlprojekt/wsgi.py
+++ wohlfuehlprojekt/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wohlfühlprojekt.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wohlfuehlprojekt.settings")
 
 application = get_wsgi_application()
